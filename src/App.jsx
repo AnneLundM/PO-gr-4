f4a9640239b4d3e09ab37b04206609f7732b3cee
TGudik <144559662+TGudik@users.noreply.github.com>
2026-05-27 12:48:02 +0200

    React opsætning

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Outlet } from 'react-router'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: "products",
+                element: <Products />
+            }
+        ]
+    }
+])
+
+export default router
