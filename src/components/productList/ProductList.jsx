86ce0e5cbff0aa75ab5f237bbc235e5a02776044
TGudik <144559662+TGudik@users.noreply.github.com>
2026-05-27 13:30:33 +0200

    fetch produkter

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,6 +1,38 @@
 import styles from "./productList.module.css"
+import { useEffect, useState } from "react";
 
 export default function ProductList() {
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    async function fetchProducts() {
+
+          try {
+              setLoading(true)
+              
+              const res = await fetch("https://gowala-t3pes.ondigitalocean.app/products");
+              
+              const resJSON = await res.json()
+              
+              const data = resJSON.data
+
+              setProducts(data)
+              
+              console.log(products)
+              setLoading(false)
+              
+            } catch (error) {
+                setError(error)
+            }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+
     return (
         <h1>ProductList</h1>
     )
