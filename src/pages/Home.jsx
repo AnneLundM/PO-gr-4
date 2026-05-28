bfd987984fc77594a272a5d07aac9cc19b67b9ae
RasmusLeddyr <rasmus-schultz@proton.me>
2026-05-28 11:16:23 +0200

    fix: home page imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
 import ProductList from "../components/productList/ProductList";
-import hero from "../components/hero";
 
 export default function Home() {
-
-
-    return (
-        <div>
-            <ProductList />
-        </div>
-    )
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
 }
