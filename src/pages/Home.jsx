170ec66cfef3813a36358b5c4d4b43ba35dc9f08
hassanhasu18-bob <hassan.hasu.18@gmail.com>
2026-05-28 12:56:34 +0200

    fix: home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import ProductList from "../components/productList/ProductList";
+import Newsletter from "../components/common/Newsletter";
+import Footer from "../components/common/Footer";
 
 export default function Home() {
   return (
     <>
       <ProductList />
+      <Newsletter />
+      <Footer />
     </>
   );
 }
