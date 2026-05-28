418ba0d3f9876f9d4c98ac844be00f05dd68f47e
TGudik <144559662+TGudik@users.noreply.github.com>
2026-05-28 13:25:45 +0200

    pointer cursor på button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import ProductList from "../components/productList/ProductList";
 import Newsletter from "../components/newsletter/Newsletter";
-import Footer from "../components/footer/Footer";
 
 export default function Home() {
   return (
