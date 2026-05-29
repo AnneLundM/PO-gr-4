7585cafd694df2b10142d6eb36451c6324724ddc
hassanhasu18-bob <hassan.hasu.18@gmail.com>
2026-05-29 11:19:28 +0200

    nyhedsbrev add

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,36 +1,36 @@
-import { useNavigate } from "react-router"
-import { Basket3 } from "react-bootstrap-icons"
-import styles from "./button.module.css"
+import { useNavigate } from "react-router";
+import { Cart } from "react-bootstrap-icons";
+import styles from "./button.module.css";
 
 export default function Button({
-    icon = false,
-    text,
-    onClick,
-    path,
-    type = "button",
-    disabled = false,
-    ariaLabel,
+  icon = false,
+  text,
+  onClick,
+  path,
+  type = "button",
+  disabled = false,
+  ariaLabel,
 }) {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    function handleClick() {
-        if (path) {
-            navigate(`/${path}`)
-        } else if (onClick) {
-            onClick()
-        }
+  function handleClick() {
+    if (path) {
+      navigate(`/${path}`);
+    } else if (onClick) {
+      onClick();
     }
+  }
 
-    return (
-        <button
-            className={styles.button}
-            onClick={handleClick}
-            type={type}
-            disabled={disabled}
-            aria-label={ariaLabel}
-        >
-            {icon && <Basket3 size={16} />}
-            <p>{text}</p>
-        </button>
-    )
+  return (
+    <button
+      className={styles.button}
+      onClick={handleClick}
+      type={type}
+      disabled={disabled}
+      aria-label={ariaLabel}
+    >
+      {icon && <Cart size={16} />}
+      <p>{text}</p>
+    </button>
+  );
 }
